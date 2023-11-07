5331f63093dd291ff949c4d754a6bd2f71fb46b1
tungmtp <75711916+tungmtp@users.noreply.github.com>
2023-11-07 21:32:52 +0700

    update UI Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
     <>
-    <Navbar></Navbar>
-    <Header></Header>
+      <Navbar></Navbar>
+      <Header></Header>
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
     </>
   );
 };
