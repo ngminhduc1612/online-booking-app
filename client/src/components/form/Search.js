7a9bf6f0a91ba5f1e886a6ed0d3398fc4bc3f3be
DinDun <dinhdung1110@gmail.com>
2023-11-11 19:26:58 +0700

    Update Search.js

--- client/src/components/form/Search.js
+++ client/src/components/form/Search.js
@@ -3,6 +3,7 @@ import {DatePicker, Select } from "antd";
 import {SearchOutlined } from "@ant-design/icons";
 import AlgoliaPlaces from   "algolia-places-react";
 import moment from "moment";
+import {useHistory} from 'react-router-dom'
 
 //destructure values from ant components
 //Phân rã cấu trúc giá trị từ các components
@@ -10,8 +11,21 @@ import moment from "moment";
 const {RangePikcer} = DatePicker;
 const { Options} = Select;
 
+//route
+const history = useHistory();
+
 const config = {
- appID: process.env.REACT_APP_ALGOLIA_APP_ID,
-apikey: process.env.REACT_APP_ALGOLIA_APP_KEY,
-language: "en"
-}
+    appID: process.env.REACT_APP_ALGOLIA_APP_ID,
+    apikey: process.env.REACT_APP_ALGOLIA_APP_KEY,
+    language: "en"
+};
+
+const Search = () => {
+    //state
+    const [location, setLocation] = useState('')
+    const [date, setDate] = useState('')
+    const [bed, setbed] = useState('')  
+
+    const handleSubmit = () => { history.push('/search-result?location=$(location)${date}&bed=${bed}')
+    };
+};
