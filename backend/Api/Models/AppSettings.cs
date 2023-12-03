b76983a89eaa0860ac52b3283ada8b163e3f0868
Moonlight200701 <93810080+Moonlight200701@users.noreply.github.com>
2023-12-03 16:55:19 +0700

    somemore work

---
+++ backend/Api/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Models
+{
+    public class AppSettings
+    {
+        public string SecretKey { get; set; }
+        public string WebRootPath { get; set; }
+    }
+}
---
+++ backend/Api/Models/UserVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Api.Models
+{
+    public class UserVM
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+    }
+}
