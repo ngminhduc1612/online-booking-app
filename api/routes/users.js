fee308270cc12ac9160063ec9f89d35aeec9d71f
ngminhduc1612 <85179226+ngminhduc1612@users.noreply.github.com>
2023-12-04 10:33:43 +0700

    Authentication
    Tạo token, sử dụng và kiểm tra tính hợp lệ của token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,19 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("hello user")
+});
+//UPDATE
+router.put("/:id", updateUser);
+//DELETE
+router.delete("/:id", deleteUser);
+//GET
+router.get("/:id", getUser);
+//GETALL
+router.get("/", getUsers);
 
 export default router
