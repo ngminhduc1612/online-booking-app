6a6fe82205e8bd7c641923ce61e9eea600fb4f57
ngminhduc1612 <85179226+ngminhduc1612@users.noreply.github.com>
2023-12-11 09:57:40 +0700

    Check roles
    Kiểm tra quyền của tài khoản để có thể thực hiện xóa hay không checkAdmin()

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,19 @@
 import express from "express"
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
     res.send("hello user")
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("hello user, you can delete your account")
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("hello admin, you are admin")
 });
 //UPDATE
 router.put("/:id", updateUser);
