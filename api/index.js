abd00369fb784559890038c3a561135b96fb1c62
DinDun <34118217+shaikowannasleep@users.noreply.github.com>
2023-12-22 12:42:09 +0700

    Init New frontend

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import  authRoute from "./routes/auth.js"
 import  usersRoute from "./routes/users.js"
 import  hotelsRoute from "./routes/hotels.js"
 import  roomsRoute from "./routes/rooms.js"
-import cookieParser from "cookie-parser"
 const app = express()
 dotenv.config()
 
@@ -23,7 +22,6 @@ mongoose.connection.on("disconnected", () => {
 })
 
 //middlewares
-app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,4 +1,6 @@
 import express from "express"
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
